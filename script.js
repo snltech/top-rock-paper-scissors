2741f2beec8c36f3490bb9c1899f391ed9eb2bc7
snltech <zaatry.m@gmail.com>
2022-03-11 15:01:33 +0200

    completed 2nd phase

--- script.js
+++ script.js
@@ -2,28 +2,35 @@
 
 const hand = ["Paper", "Scissors", "Rock"];
 
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay(){
     return hand[Math.floor(Math.random()*3)];
 }
 
 function playRound(playerSelection, computerSelection){
-    let playerHand= playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let playerHand = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if(playerHand == "Rock"){
         if(computerSelection == "Rock"){
             return "It's a Tie!";
         }
         else if(computerSelection == "Scissors"){
+            playerScore += 1;
             return "You Win! Rock beats Scissors";
         }
         else{
+            computerScore += 1;
             return "You Lose! Paper beats Rock";
         }
     }
     else if(playerHand == "Paper"){
         if(computerSelection == "Rock"){
+            playerScore += 1;
             return "You Win! Paper beats Rock";
         }
         else if(computerSelection == "Scissors"){
+            computerScore += 1;
             return "You Lose! Scissors beats Paper";
         }
         else{
@@ -32,12 +39,14 @@ function playRound(playerSelection, computerSelection){
     }
     else if(playerHand == "Scissors"){
         if(computerSelection == "Rock"){
+            computerScore += 1;
             return "You Lose! Rock beats Scissors";
         }
         else if(computerSelection == "Scissors"){
             return "It's a Tie!";
         }
         else{
+            playerScore += 1;
             return "You Win! Scissors beats Paper";
         }
     }
@@ -46,8 +55,23 @@ function playRound(playerSelection, computerSelection){
 function game(e){
         let playerSelection = this.getAttribute('value');
         let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
+        document.querySelector('.result').innerHTML = playRound(playerSelection, computerSelection);
+        document.querySelector('.score').textContent = `Player: ${playerScore} vs Computer: ${computerScore}`;
+        if (computerScore == 5)
+        {
+            document.querySelector('.result').innerHTML = `Computer Wins! <br>Start a new game`;
+            computerScore = 0;
+            playerScore = 0;
+        }
+        if (playerScore == 5)
+        {
+            document.querySelector('.result').innerHTML = `Player Wins! <br>Start a new game`;
+            computerScore = 0;
+            playerScore = 0;
+        }
 }
+
+document.querySelector('.score').textContent = `player: ${playerScore} vs computer: ${computerScore}`;
 
 const btns = document.querySelectorAll('button');
 btns.forEach(btn => btn.addEventListener('click', game));
